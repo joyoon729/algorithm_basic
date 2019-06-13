9631d5033aaf37d8e5c5b03bc13bab21abf489bc
joyoon <joyoon729@gmail.com>
2019-06-13 18:08:07 +0900

    190613 | fix quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -2,53 +2,53 @@
 #include <vector>
 using namespace std;
 
-// /* swap
-// ----------------------------------------------------*/
-// void swap(int& a, int& b){
-// 	int tmp;
-// 	tmp = b;
-// 	b = a;
-// 	a = tmp;
-// }
+/* swap
+----------------------------------------------------*/
+void swap(int& a, int& b){
+	int tmp;
+	tmp = b;
+	b = a;
+	a = tmp;
+}
 
-// /* quick Sort class
-// ----------------------------------------------------*/
-// class quickS{
-// private:
-// 	int n;
-// 	vector<int> num;
-// public:
-// 	// 테스트 벡터 생성
-// 	quickS(){
-// 		// [5 3 8 4 9 1 6 2 7 0]
-// 		num.push_back(5);num.push_back(3);num.push_back(8);
-// 		num.push_back(4);num.push_back(9);num.push_back(1);
-// 		num.push_back(6);num.push_back(2);num.push_back(7);
-// 		num.push_back(0);
-// 		n = vector.size();
-// 	}
-// 	quickS(int n, vector<int> num):n(n), num(num){}
-// 	void print();
-// };
-// void quickS::print(){
-// 	for(int i : num){
-// 		cout << i ;
-// 		// if(i==num.at(num.size()-1)) cout << endl;
-// 	}
-// }
+/* quick Sort class
+----------------------------------------------------*/
+class quickS{
+private:
+	int n;
+	vector<int> num;
+public:
+	// 테스트 벡터 생성
+	quickS(){
+		// [5 3 8 4 9 1 6 2 7 0]
+		num.push_back(5);num.push_back(3);num.push_back(8);
+		num.push_back(4);num.push_back(9);num.push_back(1);
+		num.push_back(6);num.push_back(2);num.push_back(7);
+		num.push_back(0);
+		n = vector.size();
+	}
+	quickS(int n, vector<int> num):n(n), num(num){}
+	void print();
+};
+void quickS::print(){
+	for(int i : num){
+		cout << i ;
+		// if(i==num.at(num.size()-1)) cout << endl;
+	}
+}
 
-// /* main
-// ----------------------------------------------------*/
-// int main(){
-// 	int n;
-// 	cin >> n;
-// 	vector<int> num;
-	
-// 	quickS q;
-// }
-
+/* main
+----------------------------------------------------*/
 int main(){
-	vector<int> v;
-	v.push_back(1);
-	cout << v.at(v.size()-1) << endl;
+	int n;
+	cin >> n;
+	vector<int> num;
+	
+	quickS q;
+}
+
+// int main(){
+// 	vector<int> v;
+// 	v.push_back(1);
+// 	cout << v.at(v.size()-1) << endl;
 }
